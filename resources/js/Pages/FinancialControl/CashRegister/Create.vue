1765a5fb37a234aaafa262d83bfcad12d8893b92
Miguel Vázquez <miguelvz26.mv@gmail.com>
2025-10-05 19:16:53 -0600

    revision final
    • beradcrubs

--- resources/js/Pages/FinancialControl/CashRegister/Create.vue
+++ resources/js/Pages/FinancialControl/CashRegister/Create.vue
@@ -7,7 +7,6 @@ import InputError from '@/Components/InputError.vue';
 
 const home = ref({ icon: 'pi pi-home', url: route('dashboard') });
 const breadcrumbItems = ref([
-    { label: 'Control Financiero', url: route('financial-control.index') },
     { label: 'Cajas', url: route('cash-registers.index') },
     { label: 'Crear' }
 ]);
