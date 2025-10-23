e8ecba6a1d036a7543b8840efb920a1b044d5309
Miguel Vázquez <miguelvz26.mv@gmail.com>
2025-10-23 13:05:06 -0600

    Mejoras varias
    • impresión de estado de cuenta de clientes

--- routes/web/customers.php
+++ routes/web/customers.php
@@ -7,5 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::post('customers/{customer}/payments', [CustomerPaymentController::class, 'store'])->name('customers.payments.store');
     Route::post('customers/batch-destroy', [CustomerController::class, 'batchDestroy'])->name('customers.batchDestroy');
+
+    // Nueva ruta para el estado de cuenta
+    Route::get('customers/{customer}/print-statement', [CustomerController::class, 'printStatement'])->name('customers.printStatement');
+
     Route::resource('customers', CustomerController::class);
 });
